447237cb1ceb273d0e9c1ab63bf82e737aa2cd57
Juan Zambrano <jpablozambrano@hotmail.com>
2023-04-10 17:43:00 -0600

    ScriptableObjects en una lista

--- TC2005_Proyecto2/Assets/Scripts/ButtonController.cs
+++ TC2005_Proyecto2/Assets/Scripts/ButtonController.cs
@@ -8,8 +8,11 @@ public class ButtonController : MonoBehaviour
 {
     [SerializeField]
     private Button _boton;
-    //_boton.onClick.AddListener(OnClick);
+    [SerializeField] 
+    private CartaSO[] _cartasSO;
 
+    private List<CartaSO> _listaCartas = new List<CartaSO>();
+    
     private void OnClick()
     {
         Debug.Log("El botón fue presionado!");
@@ -20,5 +23,36 @@ public class ButtonController : MonoBehaviour
     {
       Assert.IsNotNull(_boton, "Falta agregar el boton");  
       _boton.onClick.AddListener(OnClick);
+      for (int i = 0; i < _cartasSO.Length; i++)
+      {
+        _listaCartas.Add(_cartasSO[i]);
+      }
     }
+    
 }
+
+
+/*
+    [SerializeField]
+    private CartaSO _datosBorreguito;
+    [SerializeField]
+    private CartaSO _datosCaballito;
+    [SerializeField]
+    private CartaSO _datosConejito;
+    [SerializeField]
+    private CartaSO _datosFoquita;
+    [SerializeField]
+    private CartaSO _datosGatito;
+    [SerializeField]
+    private CartaSO _datosHuroncito;
+    [SerializeField]
+    private CartaSO _datosOsito;
+    [SerializeField]
+    private CartaSO _datosPandita;
+    [SerializeField]
+    private CartaSO _datosPerrito;
+    [SerializeField]
+    private CartaSO _datosPollito;
+    //List<CartaSO> _listaSO = new List<CartaSO>{_datosBorreguito, _datosCaballito, _datosConejito, _datosFoquita, _datosGatito, _datosHuroncito, _datosOsito, _datosPandita, _datosPerrito, _datosPollito};
+    List<CartaSO> _listaSO = new List<CartaSO>;
+    _listaSO.add(_datosBorreguito);*/
